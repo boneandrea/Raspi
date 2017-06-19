301909c128f255b98babc0464125f0344d469ea2
Your Name <you@example.com>
2017-06-20 08:02:09 +0900

    app.debug=Trueの時だけログを吐くことを思い出した.死ぬまでdebug. quit できるようになった.

--- srv.py
+++ srv.py
@@ -12,10 +12,10 @@ import os
 import logging # log
 from logging.handlers import RotatingFileHandler #log
 
-show_command="show_news"
+show_command="/home/pi/bin/show_news"
 
 app = Flask(__name__)
-
+#app.debug=True
 
 @app.route('/')
 def hello_world():
@@ -23,17 +23,20 @@ def hello_world():
 
 @app.route('/ja')
 def hello_world_ja():
+    app.logger.info("hello world")
     return 'こんにちは 世界！'
 
 @app.route('/news', methods=['POST'])
 def get_news():
     pp = pprint.PrettyPrinter(indent=4)
+    app.logger.info(pp)
     if request.form["str"] != "":
         str=request.form["str"]
 
-        #Hl: "/voice/voice_data.php",
+        app.logger.info(str)
+
         data= {"message": "えっ　なんだって？"}
-        # ここでエンコードして文字→バイトにする！
+        # ここでエンコードして文字 => バイトにする！
         data_encoded = urllib.parse.urlencode(data).encode("utf-8")
         with urllib.request.urlopen("http://192.168.207.42/voice/voice_data.php", data=data_encoded) as res:
             html = res.read().decode("utf-8")
@@ -43,9 +46,8 @@ def get_news():
             #run(>=3.5)
         res = subprocess.call(["sudo", "killall", "demo"])
         res = subprocess.call([show_command, str])
-        print (res)
-	
-#    pp.pprint( "body: {0}".format(request.data ))
+        app.logger.info(res)
+
     return 'set'
 
 @app.route('/reset')
@@ -54,11 +56,18 @@ def reset_news():
     return "reset"
 
 
+# 終了
+# https://stackoverflow.com/questions/15562446/how-to-stop-flask-application-without-using-ctrl-c
+
 @app.route('/quit')
 def quit():
     app.logger.info("QUIT..抜けられない")
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
     return "quit"
-#    app.terminate()
+
 
 
 def not_exist_makedirs(path):
@@ -80,9 +89,8 @@ if __name__ == '__main__':
     )
     
     debug_file_handler.setLevel(logging.INFO)
-#    debug_file_handler.setFormatter(formatter)
+    debug_file_handler.setFormatter(formatter)
     app.logger.addHandler(debug_file_handler)
-
 
     # handler = RotatingFileHandler('foo.log', maxBytes=10000, backupCount=1) # log
     # handler.setLevel(logging.DEBUG) # log
